5e578d8abea0da98331384c918338eae3d7a91c1
Dao Pham <daopm.dean@gmail.com>
2021-10-23 16:07:26 +0700

    lift app state to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:expense_tracker/models/transaction.dart';
 import 'package:expense_tracker/widgets/new_transaction.dart';
 import 'package:expense_tracker/widgets/transaction_list.dart';
-import 'package:expense_tracker/widgets/user_transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,7 +21,17 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _transactions = [
+    Transaction(id: '1', title: 'Food', amount: 12, date: DateTime.now()),
+    Transaction(id: '2', title: 'Drink', amount: 4, date: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,7 +40,7 @@ class MyHomePage extends StatelessWidget {
         actions: [
           IconButton(
             icon: Icon(Icons.add),
-            onPressed: () {},
+            onPressed: () => startNewTransaction(context),
           ),
         ],
       ),
@@ -46,14 +55,39 @@ class MyHomePage extends StatelessWidget {
               elevation: 15,
             ),
           ),
-          UserTransaction(),
+          TransactionList(
+            transactions: _transactions,
+          ),
         ],
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () => startNewTransaction(context),
       ),
     );
+  }
+
+  void startNewTransaction(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (_) {
+        return NewTransaction(addTransaction: _newTransaction);
+      },
+    );
+  }
+
+  void _newTransaction(String title, double amount) {
+    var uuid = Uuid();
+    var transaction = Transaction(
+      id: uuid.v1(),
+      title: title,
+      amount: amount,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transactions.add(transaction);
+    });
   }
 }
