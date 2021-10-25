9d3cde65f92822d4f54bcd37cce89c6df7995821
Dao Pham <daopm.dean@gmail.com>
2021-10-25 21:55:47 +0700

    update date & time picker for add new transaction

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function addTransaction;
@@ -11,10 +12,10 @@ class NewTransaction extends StatefulWidget {
 
 class _NewTransactionState extends State<NewTransaction> {
   final titleController = TextEditingController();
-
   final amountController = TextEditingController();
+  DateTime selectedDate;
 
-  void submit() {
+  void _submit() {
     var enteredTitle = titleController.text;
     var enteredAmount = double.tryParse(amountController.text);
 
@@ -32,6 +33,45 @@ class _NewTransactionState extends State<NewTransaction> {
     );
 
     Navigator.of(context).pop();
+  }
+
+  void _showDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2015),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      if (selectedDate != null) {
+        selectedDate = pickedDate.add(Duration(
+          hours: selectedDate.hour,
+          minutes: selectedDate.minute,
+        ));
+        _showTimePicker();
+        return;
+      }
+      selectedDate = pickedDate;
+      _showTimePicker();
+    });
+  }
+
+  void _showTimePicker() {
+    showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    ).then((pickedTime) {
+      setState(() {
+        if (pickedTime == null) {
+          return;
+        }
+        selectedDate = selectedDate.add(
+          Duration(hours: pickedTime.hour, minutes: pickedTime.minute),
+        );
+      });
+    });
   }
 
   @override
@@ -53,13 +93,37 @@ class _NewTransactionState extends State<NewTransaction> {
               keyboardType: TextInputType.numberWithOptions(
                 decimal: true,
               ),
-              onSubmitted: (_) => submit(),
+              onSubmitted: (_) => _submit(),
             ),
-            TextButton(
-              onPressed: submit,
+            Container(
+              height: 70,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Text(selectedDate == null
+                        ? 'No date chosen!'
+                        : '${DateFormat().format(selectedDate)}'),
+                  ),
+                  TextButton(
+                    onPressed: _showDatePicker,
+                    child: Text(
+                      'Choose Date',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    style: TextButton.styleFrom(
+                      primary: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ElevatedButton(
+              onPressed: _submit,
               child: Text('Add'),
               style: TextButton.styleFrom(
-                primary: Colors.purple,
+                primary: Theme.of(context).textTheme.button.color,
               ),
             )
           ],
